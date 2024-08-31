59044d9d3248fbf38f50e50380947b4fb80f2ebb
Dinesh <dineshwbd@gmail.com>
2024-08-31 19:53:22 +0530

    rel

--- att-website/src/views/HomeView.vue
+++ att-website/src/views/HomeView.vue
@@ -139,10 +139,10 @@
       </div>
       <ul class="shape-group-7 list-unstyled">
         <li class="shape shape-1"><img src="../assets/img/circle-1.png" alt="Line"></li>
-        <li class="shape shape-2"><img src="../assets/img/bubble-2.png" alt="Line"></li>
-        <li class="shape shape-5"><img src="../assets/img/bubble-2.png" alt="Line"></li>
+        <li class="shape shape-2"><img src="../assets/img/bubble-15.png" alt="Line"></li>
         <li class="shape shape-3"><img src="../assets/img/bubble-1.png" alt="Line"></li>
         <li class="shape shape-4"><img src="../assets/img/we-do.png" alt="Line"></li>
+        <li class="shape shape-5"><img src="../assets/img/bubble-2.png" alt="Line"></li>
       </ul>
     </div>
 
