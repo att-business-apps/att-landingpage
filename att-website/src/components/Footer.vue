b661d0b09a366b11a4c3afadab324c8d1fb6b718
Dinesh <dineshwbd@gmail.com>
2025-09-29 06:35:21 +0530

    PageSpeed

--- att-website/src/components/Footer.vue
+++ att-website/src/components/Footer.vue
@@ -58,22 +58,22 @@
           <ul class="list-unstyled">
             <li>
               <a href="https://in.linkedin.com/company/amortree-tech" target="_blank">
-                <img src="../assets/img/icon/sm-l.png" width="50" alt="amortree linkedin" />
+                <img src="../assets/img/icon/sm-l.png" alt="amortree linkedin" />
               </a>
             </li>
             <li>
               <a href="https://www.instagram.com/amortreetech/" target="_blank">
-                <img src="../assets/img/icon/sm-i.png" width="50" alt="amortree instagram" />
+                <img src="../assets/img/icon/sm-i.png" alt="amortree instagram" />
               </a>
             </li>
             <li>
               <a href="https://www.facebook.com/amortreetech/" target="_blank">
-                <img src="../assets/img/icon/sm-f.png" width="50" alt="amortree facebook" />
+                <img src="../assets/img/icon/sm-f.png" alt="amortree facebook" />
               </a>
             </li>
             <li>
               <a href="https://dribbble.com/amortreetech" target="_blank">
-                <img src="../assets/img/icon/sm-d.png" width="50" alt="amortree dribbble" />
+                <img src="../assets/img/icon/sm-d.png" alt="amortree dribbble" />
               </a>
             </li>
           </ul>
